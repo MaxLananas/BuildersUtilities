7c8dd6dcd41e360835578ac66aa8d94af856b45d
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-07-18 14:33:14 -0400

    Update AbstractLangConfig.java

--- src/main/java/dev/tehbrian/agna/paper/configurate/AbstractLangConfig.java
+++ src/main/java/dev/tehbrian/agna/paper/configurate/AbstractLangConfig.java
@@ -15,15 +15,15 @@ public abstract class AbstractLangConfig<W extends ConfigurateWrapper<?>>
 	}
 
 	@Override
-	public void load() throws ConfigurateException {
+	public final void load() throws ConfigurateException {
 		this.wrapper().load();
 	}
 
-	public Component c(final NodePath path) {
+	public final Component c(final NodePath path) {
 		return MiniMessage.miniMessage().deserialize(this.getAndVerifyString(path));
 	}
 
-	protected String getAndVerifyString(final NodePath path) {
+	protected final String getAndVerifyString(final NodePath path) {
 		final String value = this.wrapper().rootNode().node(path).getString();
 		return value != null ? value : "";
 	}
