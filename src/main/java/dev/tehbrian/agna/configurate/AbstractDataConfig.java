d3c97009b29ca967f2f5d6ff0bfb5cea08468db4
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-07-18 14:19:17 -0400

    Create AbstractDataConfig.java

---
+++ src/main/java/dev/tehbrian/agna/configurate/AbstractDataConfig.java
@@ -0,0 +1,29 @@
+package dev.tehbrian.agna.configurate;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.spongepowered.configurate.ConfigurateException;
+
+public abstract class AbstractDataConfig<W extends ConfigurateWrapper<?>, D>
+		extends AbstractConfig<W> {
+
+	private @Nullable D data;
+
+	protected AbstractDataConfig(final W wrapper) {
+		super(wrapper);
+	}
+
+	protected abstract Class<D> dataClass();
+
+	@Override
+	public void load() throws ConfigurateException {
+		this.wrapper().load();
+		this.data = this.wrapper().rootNode().get(this.dataClass());
+	}
+
+	public D data() {
+		if (this.data == null) {
+			throw new IllegalStateException("Config has not been loaded yet");
+		}
+		return this.data;
+	}
+}
