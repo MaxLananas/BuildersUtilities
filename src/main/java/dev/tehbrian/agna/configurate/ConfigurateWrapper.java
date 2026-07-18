502c1500d5a807bcf6c5c487081b3be5785ae563
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-07-18 13:27:01 -0400

    Create ConfigurateWrapper.java

---
+++ src/main/java/dev/tehbrian/agna/configurate/ConfigurateWrapper.java
@@ -0,0 +1,37 @@
+package dev.tehbrian.agna.configurate;
+
+import org.spongepowered.configurate.CommentedConfigurationNode;
+import org.spongepowered.configurate.ConfigurationLoader;
+import org.spongepowered.configurate.ConfigurateException;
+
+import java.nio.file.Path;
+
+public class ConfigurateWrapper<T extends ConfigurationLoader<CommentedConfigurationNode>> {
+
+	private final Path path;
+	private final T loader;
+	private CommentedConfigurationNode rootNode;
+
+	public ConfigurateWrapper(final Path path, final T loader) {
+		this.path = path;
+		this.loader = loader;
+	}
+
+	public void load() throws ConfigurateException {
+		this.rootNode = this.loader.load();
+	}
+
+	public void save() throws ConfigurateException {
+		if (this.rootNode != null) {
+			this.loader.save(this.rootNode);
+		}
+	}
+
+	public CommentedConfigurationNode rootNode() {
+		return this.rootNode;
+	}
+
+	public Path path() {
+		return this.path;
+	}
+}
